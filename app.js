288690335e0a55125b88e043c709a0ae603dd0d0
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-21 22:18:14 +0530

    working on api doc

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
 
-const port = 7800;
+const port = process.env.PORT || 7800;
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const mongoUrl = process.env.MongoURL;
+// const cors = require('cors');
+// let bodyParser = require('body-parser')
+// let db;
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello from express')
+    res.send('Hello from Express')
 })
 
-app.get('/product', (req,res)=>{
-    res.send('This is from Product')
+app.get('/products', (req,res)=>{
+    db.collection('product').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
 })
+// app.get('/categories', (req,res)=>{
+//     db.collection('product').find().toArray((err,result) => {
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
+//connection with db
+MongoClient.connect(mongoUrl,(err,client)=>{
+    if(err) console.log('Error while connecting');
+    db = client.db('solulab-restapi');
+    app.listen(port, ()=>{
+        console.log(`Server is running on port ${port}`)
+    })
 })
