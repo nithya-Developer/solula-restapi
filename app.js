09dd4600f1e2bab54efb9da9d665cec93330925d
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-22 19:56:41 +0530

    task completed

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ dotenv.config()
 const port = process.env.PORT || 7800;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
-const mongoUrl = process.env.MongoURL;
-// const cors = require('cors');
+const mongoUrl = process.env.LiveMongo;
 const bodyParser = require('body-parser')
 let db;
 
@@ -35,7 +34,25 @@ app.get('/products', (req,res)=>{
         res.send(result)
     })
 })
-
+//update product
+app.put('/updateProduct/:id', (req, res) => {
+    let pid = Number(req.params.id);
+    db.collection('product').updateOne(
+        {product_id:pid},
+        {
+            $set:{
+                "product_name":req.body.product_name,
+                "qtyPerUnit":req.body.qty_per_unit,
+                "unitPrice":req.body.unit_price,
+                "unitInStock":req.body.unit_in_stock,
+                "discontinued":req.body.discontinued
+            }
+        }, (err, result) => {
+            if (err) throw err;
+            res.send('Product Updated')
+        }
+    )
+})
 // create product
 app.post('/addProducts', (req,res) => {
     db.collection('product').insert(req.body,(err,result) => {
@@ -44,6 +61,14 @@ app.post('/addProducts', (req,res) => {
     })
 })
 
+// delete one record of the product by its ID
+app.delete('/deleteProduct/:id', (req, res) => {
+    let _id = mongo.ObjectId(req.params.id);
+    db.collection('product').remove({_id},(err,result) => {
+        if(err) throw err;
+        res.send('Product Removed')
+    })
+})
 
 //List of all category
 app.get('/categories', (req,res)=>{
@@ -61,7 +86,16 @@ app.post('/addCategories', (req,res) => {
     })
 })
 
-//connection with db
+// delete one record of the category by its ID
+app.delete('/deleteCategory/:id', (req, res) => {
+    let _id = mongo.ObjectId(req.params.id);
+    db.collection('category').remove({_id},(err,result) => {
+        if(err) throw err;
+        res.send('Category Removed')
+    })
+})
+
+//connection with database
 MongoClient.connect(mongoUrl,(err,client)=>{
     if(err) console.log('Error while connecting');
     db = client.db('solulab-restapi');
